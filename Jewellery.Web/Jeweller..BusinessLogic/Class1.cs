55f2d596eccb30b6bc3d18cf8b42e08cc7e4b720
unknown <sankirnarana@gmail.com>
2014-05-27 23:53:55 +0530

    no message

---
+++ Jewellery.Web/Jeweller..BusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeweller._.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
