11f1f48ac175cf2188acb441a11c8513e267ea2a
unknown <sankirnarana@gmail.com>
2014-06-01 18:47:51 +0530

    no message

--- Jewellery.Web/Jewellery.Web/App_Start/BundleConfig.cs
+++ Jewellery.Web/Jewellery.Web/App_Start/BundleConfig.cs
@@ -20,32 +20,7 @@ namespace Jewellery.Web
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                        "~/Scripts/knockout-{version}.js",
-                        "~/Scripts/knockout.mapping*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/layout").Include(
-            //            "~/Scripts/bootstrap*",
-            //            "~/Content/Plugins/breakpoints/breakpoints.js",
-            //            "~/Scripts/Shared/Common*",
-            //            "~/Scripts/Shared/Url*",
-            //            "~/Scripts/Shared/DateFormat*",
-            //            "~/Scripts/jquery.loadTemplate-*"
-            //            ));
-
-            bundles.Add(new ScriptBundle("~/Content/assets/js").Include(
-             "~/Content/assets/plugins/jquery-1.8.3.min.js",
-             "~/Content/assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js",
-             "~/Content/assets/plugins/bootstrap/js/bootstrap.min.js",
-             "~/Content/assets/plugins/breakpoints/breakpoints.js",
-             "~/Content/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
-             "~/Content/assets/plugins/jquery.blockui.js",
-             "~/Content/assets/plugins/jquery.cookie.js",
-             "~/Content/assets/plugins/uniform/jquery.uniform.min.js",
-             "~/Content/assets/scripts/app.js",
-             "~/Content/Uploadify/swfobject.js",
-              "~/Content/Uploadify/jquery.uploadify.v2.1.4.min.js"
-             ));
+         
 
             #region "Kendo UI Bundle(s)"
 
@@ -76,27 +51,7 @@ namespace Jewellery.Web
 
             #region "Style"
 
-            //bundles.Add(new StyleBundle("~/Content/layout").Include(
-            //    "~/Content/bootstrap*",
-            //    "~/Content/Plugins/font-awesome/css/font-awesome*",
-            //    "~/Content/Site*",
-            //    "~/Content/Search.css",
-            //    "~/Content/Plugins/kendoui/styles/kendo.common*",
-            //    "~/Content/Plugins/kendoui/styles/kendo.flat*"));
             
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.core.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.button.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.slider.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Areas/Admin/Content/themes/base/jquery.ui.theme.css"));
 
             //######################## Start LOGIN ASSETS #######################
             bundles.Add(new StyleBundle("~/Content/assets/css/Login").Include(
@@ -113,18 +68,35 @@ namespace Jewellery.Web
 
 
 
+            #region admin Assest
+            
+            #endregion
+
             bundles.Add(new StyleBundle("~/Content/assets/css").Include(
-                   "~/Areas/Admin/Content/assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.css",
-                   "~/Areas/Admin/Content/assets/plugins/bootstrap/css/bootstrap.min.css",
-                   "~/Areas/Admin/Content/assets/plugins/bootstrap/css/bootstrap-responsive.min.css",
-                   "~/Areas/Admin/Content/assets/plugins/font-awesome/css/font-awesome.min.css",
-                   "~/Areas/Admin/Content/assets/css/style-metro.css",
-                   "~/Areas/Admin/Content/assets/css/style.css",
-                   "~/Areas/Admin/Content/assets/css/style-responsive.css",
-                   "~/Areas/Admin/Content/assets/css/themes/default.css",
-                   "~/Areas/Admin/Content/assets/plugins/uniform/css/uniform.default.css"
+                  "~/Areas/Admin/Content/assets/plugins/bootstrap/css/bootstrap.min.css",
+"~/Areas/Admin/Content/assets/plugins/bootstrap/css/bootstrap-responsive.min.css",
+"~/Areas/Admin/Content/assets/plugins/font-awesome/css/font-awesome.min.css",
+"~/Areas/Admin/Content/assets/css/style-metro.css",
+"~/Areas/Admin/Content/assets/css/style.css",
+"~/Areas/Admin/Content/assets/css/style-responsive.css",
+"~/Areas/Admin/Content/assets/css/themes/default.css",
+"~/Areas/Admin/Content/assets/plugins/uniform/css/uniform.default.css"
                  
                    ));
+
+            bundles.Add(new ScriptBundle("~/Content/assets/js").Include(
+           "~/Areas/Admin/Content/assets/plugins/jquery-1.8.3.min.js",
+"~/Areas/Admin/Content/assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js",
+"~/Areas/Admin/Content/assets/plugins/bootstrap/js/bootstrap.min.js",
+"~/Areas/Admin/Content/assets/plugins/breakpoints/breakpoints.js",
+"~/Areas/Admin/Content/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+"~/Areas/Admin/Content/assets/plugins/jquery.blockui.js",
+"~/Areas/Admin/Content/assets/plugins/jquery.cookie.js",
+"~/Areas/Admin/Content/assets/plugins/uniform/jquery.uniform.min.js",
+"~/Areas/Admin/Content/assets/scripts/app.js"
+
+
+                ));
 
             #endregion "Style"
 
